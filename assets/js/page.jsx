fddcc0b1fb33446c40d1cbb64f661a4d20046711
hongjie221 <1091840333@qq.com>
2019-11-19 19:32:31 -0500

    add login sign up form

--- assets/js/page.jsx
+++ assets/js/page.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, NavLink, Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Link
+} from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import { Provider, connect } from "react-redux";
+import Login from "./login";
+import SignUp from "./signup";
+import store from "./store";
 
 export default function init_page(root) {
-  ReactDOM.render(<Page />, root);
+  let tree = (
+    <Provider store={store}>
+      <Page />
+    </Provider>
+  );
+  ReactDOM.render(tree, root);
 }
 
 function Page(props) {
@@ -18,7 +33,12 @@ function Page(props) {
             </NavLink>
           </Nav.Item>
           <Nav.Item>
-            <NavLink to="/users" exact activeClassName="active" className="nav-link">
+            <NavLink
+              to="/users"
+              exact
+              activeClassName="active"
+              className="nav-link"
+            >
               Users
             </NavLink>
           </Nav.Item>
@@ -27,12 +47,21 @@ function Page(props) {
 
       <Switch>
         <Route exact path="/">
-          <h1>Home</h1>
+          <Login />
+          <NavLink
+            to="/sign_up"
+            exact
+            activeClassName="active"
+            className="nav-link"
+          >
+           Don't have an account? Sign Up Now!
+          </NavLink>
         </Route>
 
-        <Route exact path="/users">
-          <h1>Users</h1>
+        <Route exact path="/sign_up">
+          <SignUp />
         </Route>
+
       </Switch>
     </Router>
   );
---
+++ assets/js/signup.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { connect } from 'react-redux';
+import { Form, Button, Alert} from 'react-bootstrap';
+import { Redirect } from 'react-router';
+
+//import { submit_login } from './ajax';
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            redirect: null,
+        };
+    }
+
+    redirect(path) {
+        this.setState({
+            redirect: path,
+        });
+    }
+
+    changed(data) {
+        this.props.dispatch({
+            type: 'SIGN_UP',
+            data: data,
+        });
+    }
+
+    render() {
+        if (this.state.redirect) {
+            return <Redirect to={this.state.redirect} />
+        }
+
+        let {email, password, name, gender, description, errors} = this.props;
+        let error_msg = null;
+        if (errors) {
+            error_msg = <Alert variant="danger">{ errors }</Alert>
+        }
+        return (
+            <div>
+              <h1>Log In</h1>
+              { error_msg }
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="text" onChange={
+                  (ev) => this.changed({email: ev.target.value})} />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" onChange={
+                  (ev) => this.changed({password: ev.target.value})} />
+              </Form.Group>
+              <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" onChange={
+                  (ev) => this.changed({name: ev.target.value})} />
+              </Form.Group>
+              <Form.Group controlId="gender">
+                <Form.Label>Gender</Form.Label>
+                <Form.Control as="select" onChange={
+                  (ev) => this.changed({gender: ev.target.value})}>
+                      <option>Male</option>
+                      <option>Female</option>
+                  </Form.Control>
+              </Form.Group>
+              <Form.Group controlId="desc">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" onChange={
+                  (ev) => this.changed({description: ev.target.value})} />
+              </Form.Group>
+              <Form.Group controlId="submit">
+                <Button variant="primary" onClick={() => submit_login(this)}>
+                  Sign Up
+                </Button>
+              </Form.Group>
+            </div>
+          );
+    }
+}
+
+function state2props(state) {
+    return state.signup;
+}
+
+export default connect(state2props)(SignUp);
